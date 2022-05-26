cae3ab9908b7e8f51a627a5f1138a62699c892ac
Florin <florinmihu31@gmail.com>
2022-05-26 08:38:17 +0300

    Add ex2

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/general/Constants.java
@@ -0,0 +1,25 @@
+package ro.pub.cs.systems.eim.practicaltest02.general;
+
+public interface Constants {
+    String BASE_URL = "http://worldtimeapi.org/api/ip";
+    /*String API_KEY = "e03c3b32cfb5a6f7069f2ef29237d87e";
+    String QUERY = "q";
+    String APP_ID = "appid";*/
+
+    /*String WEATHER = "weather";
+    String DESCRIPTION = "description";
+    String TEMP = "temp";
+    String MAIN = "main";
+    String WIND = "wind";
+    String SPEED = "speed";
+    String PRESSURE = "pressure";
+    String HUMIDITY = "humidity";
+    String UNITS = "units";
+    String METRIC = "metric";
+    String TEMPERATURE = "temperature";
+    String WIND_SPEED = "wind_speed";
+    String CONDITION = "condition";
+    String ALL = "all";*/
+
+    String LOG_TAG = "PracticalTest02";
+}
